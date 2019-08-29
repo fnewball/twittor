1a834a1ba54947f956d4f7c0a48db8d0a61ffa21
Francisco Newball <francisco.newball@gmail.com>
2019-08-28 20:13:22 -0500

    modif app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 //Service Worker
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 } 
 
 
