ba5f6455c2c1fd48196ea5f7f90a6c4a65143df1
Francisco Newball <francisco.newball@gmail.com>
2019-08-28 22:59:10 -0500

    update master

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -1,11 +1,24 @@
 
-function actualizaCacheDinamico (dynamicCache, req, res) {
-    if (res.ok) {
-        caches.open(dynamicCache).then(cache => {
-            cache.put(req, res.clone());
+
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+            
             return res.clone();
+
         });
-    }else {
+
+    } else {
         return res;
     }
+
+
+
 }
+
